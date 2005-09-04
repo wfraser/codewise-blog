58dc6d0a1412a7558660af5b29ea4a4a1cda5d1e
William R. Fraser <wrf@codewise.org>
2005-09-04 13:55:10 +0000

    Fixed content-type, removed debug output.

--- stylesheet.php
+++ stylesheet.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Content-type: text/plain");
+header("Content-type: text/css");
 
 require("l1_mysql.php");
 
@@ -8,8 +8,6 @@ $db = new L1_MySQL("localhost", "codewiseblog", "!#joltColaINaCan");
 $db->database("codewiseblog");
 
 $q = $db->issue_query("SELECT css FROM skin WHERE blogid = " . $db->prepare_value($_GET['id']));
-
-var_export($db->fetch_all($q, L1SQL_NUM));
 
 if($db->num_rows[$q] == 0 || ($text = $db->fetch_var($q)) === NULL)
     readfile("/srv/www/site/www.codewise.org/blueEye.css");
