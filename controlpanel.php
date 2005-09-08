f1494bf5125673c1f87ebf2dab9b2f6549d45ec0
William R. Fraser <wrf@codewise.org>
2005-09-08 04:55:50 +0000

    Control panel skeleton design

--- controlpanel.php
+++ controlpanel.php
@@ -8,9 +8,56 @@
 ** Copyright (c) 2005 Codewise.org
 */
 
+function cplogin()
+{
+    global $db;
+
+    $q = $db->issue_query("SELECT password FROM blogs WHERE blogid = '" . BLOGID . "'");
+    $hash = $db->fetch_var($q);
+
+    if($_SESSION['controlpanel'] || md5($_POST['password']) == $hash)
+    {
+        $_SESSION['controlpanel'] = TRUE;
+        return "You are now logged in.<br /><a href=\"" . INDEX_URL . "?controlpanel\">Continue to the Control Panel</a>";
+    } else {
+        $GLOBALS['NOTIFY'] = "Incorrect password.";
+        return main_page(1);
+    }
+}
+
 function controlpanel()
 {
-    return "<html><body>[[control panel]]</body></html>";
+    global $db;
+
+    if(!$_SESSION['controlpanel'])
+    {
+        header("Location: " . INDEX_URL . "?notloggedin");
+        return;
+    }
+
+    if(isset($_GET['zomg!']))
+    {
+        //oo
+    } else {
+        $current = "home";
+        $body = "Body goes here...";
+    }
+
+    $args = array
+    (
+        "current" => $current,
+        "cpurl"        => INDEX_URL . "?controlpanel",
+        "url_settings" => INDEX_URL . "?controlpanel:settings",
+        "url_write"    => INDEX_URL . "?controlpanel:write",
+        "url_userinfo" => INDEX_URL . "?controlpanel:userinfo",
+        "url_skin"     => INDEX_URL . "?controlpanel:skin",
+    );
+
+    $main = skinvoodoo("controlpanel", "", $args);
+
+    $out = str_replace("<!-- #CWB_CP_BODY# -->", $body, $main);
+
+    return $out;
 }
 
 ?>
