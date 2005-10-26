871e3e7ec2738db481e3b3f280a71201d82847ad
William R. Fraser <wrf@codewise.org>
2005-10-26 05:55:01 +0000

    require() cwbmulti.php for user detection

--- rdf.php
+++ rdf.php
@@ -30,84 +30,9 @@ header("Content-Type: text/xml");
 
 echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
 
-require("settings.php");
+define("NO_ACTION", TRUE);
 
-chdir(FSPATH);
-
-/*
-** Set up environment
-*/
-
-// deal with errors properly
-ini_set("track_errors", true);
-error_reporting(E_ALL ^ E_NOTICE);
-
-// clean out this crap - it's never used
-unset($HTTP_POST_VARS, $HTTP_GET_VARS, $HTTP_COOKIE_VARS, $HTTP_SERVER_VARS, $HTTP_ENV_VARS, $HTTP_POST_FILES, $HTTP_SESSION_VARS);
-
-require("misc.php");
-
-require("l1_mysql.php");
-$db = new L1_MySQL(SQL_HOST, SQL_USER, SQL_PASS);
-
-// custom error handler to mail the admin as well as print any errors
-$db->error_callback = $db->warning_callback = "mail_db_error";
-
-$db->database(SQL_DB);
-
-/*
-** Who are we running for?
-*/
-
-$q = $db->issue_query("SELECT blogid,name FROM blogs");
-$blogdata = $db->fetch_all($q, L1SQL_ASSOC, "name");
-
-if(SUBDOMAIN_MODE)
-{
-    $who = preg_replace("/\." . quotemeta(BASE_DOMAIN) . "$/", "", $_SERVER['HTTP_HOST']);
-    if($who == DEFAULT_SUBDOMAIN || $who == BASE_DOMAIN)
-        $who = "";
-} else {
-    $who = preg_replace("/^" . str_replace("/", "\/", quotemeta(INSTALLED_PATH)) . "/", "", $_SERVER['REQUEST_URI']);
-}
-
-//KEEP
-//$who = "netmanw00t";
-
-if($who == "")
-{
-    define("BLOGID", 1);
-    define("BLOGNAME", "");
-    define("INDEX_URL", "http://" . DEFAULT_SUBDOMAIN . BASE_DOMAIN . INSTALLED_PATH);
-} elseif(!isset($blogdata[$who])) {
-    die( "CodewiseBlog :: Invalid User \"$who\"" );
-} else {
-    define("BLOGID", $blogdata[$who]['blogid']);
-    define("BLOGNAME", $who);
-    define("ADMIN_EMAIL", $blogdata[$who]['email']);
-    if(SUBDOMAIN_MODE)
-        define("INDEX_URL", "http://" . BLOGNAME . "." . BASE_DOMAIN . INSTALLED_PATH);
-    else
-        define("INDEX_URL", "http://" . DEFAULT_SUBDOMAIN . BASE_DOMAIN . INSTALLED_PATH . "/" . BLOGNAME);
-}
-
-/*
-** Set up the $BLOGINFO global var with some useful stuff
-*/
-
-$q = $db->issue_query("SELECT blogid,name,email,realname,birthday,location,interests,links,photo,homepage,title FROM blogs WHERE blogid = '" . BLOGID . "'");
-$BLOGINFO = $db->fetch_row($q, 0, L1SQL_ASSOC);
-
-if($BLOGINFO['birthday'])
-    $BLOGINFO['age'] = date("Y", time() - $BLOGINFO['birthday']);
-
-$BLOGINFO['index_url'] = INDEX_URL;
-$BLOGINFO['ucp_url'] = INDEX_URL . "?controlpanel";
-
-$BLOGINFO['interests'] = nl2br($BLOGINFO['interests']);
-$BLOGINFO['links'] = nl2br($BLOGINFO['links']);
-$BLOGINFO['version'] = CWBVERSION;
-$BLOGINFO['anonymous_name'] = ANONYMOUS_NAME;
+require("cwbmulti.php");
 
 ?>
 
