1463114e994af3a1125ec6bff2c922f29e0e2cff
William R. Fraser <wrf@codewise.org>
2006-04-15 03:50:05 +0000

    removed stray var_dump()

--- register.php
+++ register.php
@@ -89,7 +89,6 @@ function check_post()
     if($_POST['birthday'] != "" && !preg_match('/^(0?[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)[0-9]{2}$/', $_POST['birthday']))
     {
         $GLOBALS['NOTIFY'] = "Invalid Birthday. Format is mm/dd/yyyy";
-        var_dump($_POST['birthday']);
         return FALSE;
     }
 
