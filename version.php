b85060a7979ca4431bef06befa8a071b99b5d76f
William R. Fraser <wrf@codewise.org>
2008-06-05 11:28:39 +0000

    try to get svn revision info for the version string

--- version.php
+++ version.php
@@ -1,6 +1,12 @@
 <?php
 
-define("CWBVERSION", "DEV-2.0.0");
+$rev = trim(`svn info | grep Revision: | sed 's/Revision: /r/'`);
+if ($rev == "" || $rev == "svn: '.' is not a working copy") {
+	define("CWBVERSION", "DEV-2.0.0");
+} else {
+	define("CWBVERSION", "DEV-2.0.0 ($rev)");
+}
+
 define("CWBTYPE", "Mainline");
 
 ?>
