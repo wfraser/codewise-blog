1f89f77be05710d46414e98614fb1043ab4c92e7
William R. Fraser <bill.fraser@gmail.com>
2009-04-20 05:22:11 -0500

    print what git revision number we're on

--- version.php
+++ version.php
@@ -1,10 +1,11 @@
 <?php
 
-$rev = trim(`svn info | grep Revision: | sed 's/Revision: /r/'`);
-if ($rev == "" || $rev == "svn: '.' is not a working copy") {
+$rev = trim(`git rev-list HEAD 2>/dev/null | wc -l`);
+
+if ($rev == 0) {
 	define("CWBVERSION", "DEV-2.0.0");
 } else {
-	define("CWBVERSION", "DEV-2.0.0 ($rev)");
+	define("CWBVERSION", "DEV-2.0.0 rev $rev");
 }
 
 define("CWBTYPE", "Mainline");
