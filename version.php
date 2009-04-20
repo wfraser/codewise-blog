b7dc47f406cc0fd0bf44e9e90ea9a53774de3c4c
William R. Fraser <bill.fraser@gmail.com>
2009-04-20 05:44:31 -0500

    describe our version scheme

--- version.php
+++ version.php
@@ -1,16 +1,35 @@
 <?php
 
+/*
+ * Codewise Blog Version Scheme:
+ *
+ * a.b.c.d
+ *
+ * a: major version
+ *        incremented with major project changes
+ * b: minor version
+ *        incremented with feature changes
+ * c: patch version
+ *        incremented with each release
+ * d: revision
+ *        number of Git commits since release
+ *        only shown on development versions
+ */
+
+// this is the current release, and also the Git tag
 define("CWBVERSIONTAG", "2.1.0");
 
+// figure out how many commits since release
 $range = CWBVERSIONTAG."..HEAD";
 $rev = trim(`git rev-list $range 2>/dev/null | wc -l`);
 
 if ($rev == 0) {
-	define("CWBVERSION", "DEV-".CWBVERSIONTAG);
+	define("CWBVERSION", CWBVERSIONTAG);
 } else {
-	define("CWBVERSION", "DEV-".CWBVERSIONTAG.".$rev");
+	define("CWBVERSION", CWBVERSIONTAG.".$rev-DEV");
 }
 
+// if you fork the project, CHANGE THIS!
 define("CWBTYPE", "Mainline");
 
 ?>
