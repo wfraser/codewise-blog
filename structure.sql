03d3c0541f68631adc84df9f707abf9a954f49d4
William R. Fraser <wfraser@delta-zero.codewise.org>
2009-04-20 04:59:23 -0500

    this is what the database should look like now

--- structure.sql
+++ structure.sql
@@ -1,81 +1,97 @@
-DROP TABLE IF EXISTS blogs;
-CREATE TABLE blogs (
-  blogid int(8) unsigned NOT NULL auto_increment,
-  name varchar(32) NOT NULL default '',
-  email varchar(64) NOT NULL default '',
-  realname varchar(64) default NULL,
-  birthday varchar(10) default NULL,
-  location varchar(255) default NULL,
-  interests text,
-  links text,
-  photo varchar(255) default NULL,
-  homepage varchar(255) default NULL,
-  title varchar(64) NOT NULL default '',
-  password varchar(32) NOT NULL default '',
-  joindate int(10) NOT NULL default '0',
-  custom_url varchar(128) default NULL,
-  skinid varchar(32) NOT NULL default '00000000000000000000000000000000',
-  status enum('validating','active','banned','closed') NOT NULL default 'validating',
-  UNIQUE KEY blogid (blogid),
-  UNIQUE KEY name (name)
-) TYPE=MyISAM;
+CREATE TABLE `blogs` (
+  `blogid` int(5) unsigned NOT NULL auto_increment,
+  `name` varchar(32) NOT NULL default '',
+  `email` varchar(63) NOT NULL default '',
+  `realname` varchar(63) default NULL,
+  `birthday` varchar(10) default NULL,
+  `location` varchar(255) default NULL,
+  `interests` text,
+  `links` text,
+  `photo` varchar(255) default NULL,
+  `homepage` varchar(255) default NULL,
+  `title` varchar(64) NOT NULL default '',
+  `password` varchar(32) NOT NULL default '',
+  `joindate` int(10) NOT NULL default '0',
+  `custom_url` varchar(128) default NULL,
+  `skinid` varchar(32) NOT NULL default '',
+  `status` enum('validating','active','banned','closed') NOT NULL default 'validating',
+  UNIQUE KEY `blogid` (`blogid`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 PACK_KEYS=0;
 
-DROP TABLE IF EXISTS imageverify;
-CREATE TABLE imageverify (
- id varchar(32) NOT NULL default '',
- text varchar(4) NOT NULL default '',
- PRIMARY KEY  (id)
-) TYPE=MyISAM;
+CREATE TABLE `imageverify` (
+  `id` int(32) unsigned NOT NULL auto_increment,
+  `text` varchar(4) NOT NULL default '',
+  `timestamp` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS replies;
-CREATE TABLE replies (
-  pid int(8) unsigned NOT NULL auto_increment,
-  tid int(8) unsigned NOT NULL default '0',
-  blogid int(8) unsigned NOT NULL default '0',
-  name varchar(128) NOT NULL default '',
-  tripcode varchar(22) default NULL,
-  timestamp int(10) unsigned NOT NULL default '0',
-  link tinytext,
-  text text NOT NULL,
-  extra text,
-  UNIQUE KEY pid (pid)
-) TYPE=MyISAM PACK_KEYS=0;
+CREATE TABLE `replies` (
+  `pid` int(8) unsigned NOT NULL auto_increment,
+  `tid` int(5) unsigned NOT NULL default '0',
+  `blogid` int(5) unsigned NOT NULL default '0',
+  `name` varchar(128) NOT NULL default '',
+  `tripcode` varchar(22) default NULL,
+  `timestamp` int(10) unsigned NOT NULL default '0',
+  `link` tinytext,
+  `text` text NOT NULL,
+  `extra` text,
+  UNIQUE KEY `pid` (`pid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 PACK_KEYS=0;
 
-DROP TABLE IF EXISTS shoutbox;
-CREATE TABLE shoutbox (
-  blogid int(8) unsigned NOT NULL default '0',
-  name varchar(128) NOT NULL default '',
-  timestamp int(10) unsigned NOT NULL default '0',
-  link tinytext,
-  text text NOT NULL,
-  extra text,
-  UNIQUE KEY timestamp (timestamp)
-) TYPE=MyISAM;
+CREATE TABLE `shoutbox` (
+  `blogid` int(5) unsigned NOT NULL default '0',
+  `name` varchar(128) NOT NULL default '',
+  `timestamp` int(10) unsigned NOT NULL default '0',
+  `link` tinytext,
+  `text` text NOT NULL,
+  `extra` text,
+  UNIQUE KEY `timestamp` (`timestamp`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS skins;
-CREATE TABLE skins (
-  skinid varchar(32) NOT NULL default '',
-  blogid int(5) unsigned NOT NULL default '0',
-  name text NOT NULL,
-  description text NOT NULL,
-  UNIQUE KEY skinid (skinid),
-  KEY blogid (blogid)
-) TYPE=MyISAM;
+CREATE TABLE `skins` (
+  `skinid` varchar(32) NOT NULL default '',
+  `blogid` int(5) unsigned NOT NULL default '0',
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `css` text,
+  `main` text,
+  `postcalendar` text,
+  `loginform` text,
+  `welcomeback` text,
+  `topic` text,
+  `controlpanel` text,
+  `replyform` text,
+  `pagelink` text,
+  `post` text,
+  `subscribeform` text,
+  `error` text,
+  `shoutbox` text,
+  `controlpanel_write` text,
+  `controlpanel_settings` text,
+  `controlpanel_edit` text,
+  `controlpanel_userinfo` text,
+  `controlpanel_skin` text,
+  `controlpanel_adduser` text,
+  `controlpanel_skin_multi` text,
+  `register` text,
+  `controlpanel_manage` text,
+  UNIQUE KEY `skinid` (`skinid`),
+  KEY `blogid` (`blogid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS subscriptions;
-CREATE TABLE subscriptions (
-  blogid int(8) unsigned NOT NULL default '0',
-  email varchar(255) NOT NULL default '',
-  password varchar(37) NOT NULL default ''
-) TYPE=MyISAM;
+CREATE TABLE `subscriptions` (
+  `blogid` int(5) unsigned NOT NULL default '0',
+  `email` varchar(255) NOT NULL default '',
+  `password` varchar(37) NOT NULL default ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS topics;
-CREATE TABLE topics (
-  tid int(8) unsigned NOT NULL auto_increment,
-  blogid int(8) unsigned NOT NULL default '0',
-  title varchar(128) NOT NULL default '',
-  timestamp int(10) unsigned NOT NULL default '0',
-  text text NOT NULL,
-  extra text,
-  UNIQUE KEY id (tid)
-) TYPE=MyISAM PACK_KEYS=0;
+CREATE TABLE `topics` (
+  `tid` int(5) unsigned NOT NULL auto_increment,
+  `blogid` int(5) unsigned NOT NULL default '0',
+  `title` varchar(128) character set utf8 NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL default '0',
+  `text` text character set utf8 collate utf8_unicode_ci NOT NULL,
+  `extra` text character set utf8,
+  UNIQUE KEY `id` (`tid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 PACK_KEYS=0;
