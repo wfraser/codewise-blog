6d85ba1737d1bb799fd148dcdff201fd4c9ccd13
William R. Fraser <bill.fraser@gmail.com>
2009-04-20 05:28:01 -0500

    I'm going to call this version 2.1.0 because I can.

--- version.php
+++ version.php
@@ -1,11 +1,14 @@
 <?php
 
-$rev = trim(`git rev-list HEAD 2>/dev/null | wc -l`);
+define("CWBVERSIONTAG", "2.1.0");
+
+$range = CWBVERSIONTAG."..HEAD";
+$rev = trim(`git rev-list $range 2>/dev/null | wc -l`);
 
 if ($rev == 0) {
-	define("CWBVERSION", "DEV-2.0.0");
+	define("CWBVERSION", "DEV-".CWBVERSIONTAG);
 } else {
-	define("CWBVERSION", "DEV-2.0.0 rev $rev");
+	define("CWBVERSION", "DEV-".CWBVERSIONTAG.".$rev");
 }
 
 define("CWBTYPE", "Mainline");
